bd5d38bf8333319376a4f84c657c8885d96be114
Chris Pittman <cpittman3@cnm.edu>
2021-11-12 06:50:15 -0600

    Completed index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 // get access to express application
 const express = require("express")
 const morgan = require("morgan")
-const bodyParser = require("bodyParser")
-const {check, validationResult} = require
+const bodyParser = require("body-parser")
+const {check, validationResult} = require("express-validator")
+const Mailgun = require("mailgun.js")
+const formData = require("form-data")
+const Recaptcha = require("express-recaptcha").RecaptchaV2
 require("dotenv").config()
 
-const app = express()
 
-// setup initial middleware
+const app = express()
+const  mailgun = new Mailgun(formData)
+const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
+const mailgunClient = mailgun.client({username: "api", key: process.env.MAILGUN_API_KEY})
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(bodyParcer.urlencoded({extended: false}))
@@ -17,7 +22,7 @@ app.use(bodyParser.json())
 const indexRoute = express.Router()
 
 const handleGetRequest = (request, response) => {
-    return response.json("The Express server is alive 😼")
+    return response.json("The express server is live 😼")
 }
 
 // Handle form validation
@@ -30,18 +35,47 @@ const validation = [
     check("email", "Please provide a valid email")
         .isEmail(),
     check("subject").optional().trim().escape(),
-    check("message", "A message is longer than 2000 required").trim().escape().isLength(min:1, Max:2000)
-
+    check("message", "A message is longer than 2000 characters is required").trim().escape().isLength({min:1, Max:2000})
     ]
 
 const handlePostRequest = (request, response) => {
-    response.append(access-control-allow-orgin, "*")
-    return response.json("Email successfully sent!")
+
+    const errors = validationResult(request)
+    response.append("access-control-allow-origin", "*")
+    console.log(request.body)
+
+    if(errors.isEmpty() === false) {
+        const currentError = errors.array()[0]
+        return response.send(`<div class="alert alert-danger" role="alert"<>strong>Oh snap</strong> ${currentError.msg}</div>`)
+    }
+
+    if(request.recaptcha.error) {
+        return response.send(`<div class='alert alert-danger' role='alert'><strong>Oh snap!</strong>There was an error with Recaptcha please try again</div>div>`
+        );
+    }
+
+    const {email, subject, name, message} = request.body
+
+    mailgunClient.messages.create(
+        process.env.MAILGUN_DOMAIN,
+        {
+           to: process.env.MAILGUN_RECIPIENT,
+           from: `${name} <postmaster@${process.env.MAILGUN_DOMAIN}>`,
+           subject: `${email}: ${subject}`,
+           text:message
+
+        }
+    ).then(() => {
+        response.send(`div class="alert alert-danger" role='alert'>Email sent successfully</div>`)
+    }).catch(error=>{
+        response.send(`div class="alert alert-danger" role='alert'><strong>Oh snap</strong>${error}</div>`)
+    })
+
 }
 
 indexRoute.route("/")
     .get(handleGetRequest)
-    .post(validation, handlePostRequest)
+    .post(validation, recaptcha.middleware.verify, handlePostRequest)
 
 app.use("/apis", indexRoute)
 
@@ -49,9 +83,3 @@ app.use("/apis", indexRoute)
 app.listen(4200, () => {
     console.log("Express server successfully built")
 })
-
-
-
-
-// implement recaptcha
-// configure and send emails using mailgun
