3e383d8bce20ed5bbd5dd3adb79e81814b47f94b
Chris Pittman <cpittman3@cnm.edu>
2021-11-22 03:03:57 -0600

    Added sfx and comments

--- public_html/js/form.js
+++ public_html/js/form.js
@@ -1,3 +1,14 @@
+/*
+  This script handles the form's animated message.
+  When the form is clicked on a short animation will play
+  and a message will appear notifying the user that I'm
+  not available for contract work. Another message will
+  appear letting the user know they can still contact me.
+  When the message (or anywhere within the gif) is clicked
+  the gif will be deleted freeing the form for use.
+*/
+
+// Delete the static dummy form and call for audio and gif to replace it
 formStart = () => {
     let formParent = document.querySelector(".formImage")
     let formChild = document.querySelector(".formAni")
@@ -5,9 +16,14 @@ formStart = () => {
     formParent.insertAdjacentHTML("beforeend", aniStage2())
 }
 
-aniStage2=() =>
-    `<img src="./images/formAni2.gif" class="formAni" onClick="formContact()" alt="Not Available">`
+// HTML for playing audio and animation
+aniStage2 = () =>
+    `  <audio 
+           src="./SFX/sfxGlass.mp3" autoplay>Your browser does not support the <code>audio</code> element.
+       </audio>
+       <img src="./images/formAni2.gif" class="formAni" onClick="formContact()" alt="Not Available">`
 
+// Delete the gif freeing the form for use
 formContact = () => {
     let formParent = document.querySelector(".formImage")
     let formChild = document.querySelector(".formAni")
